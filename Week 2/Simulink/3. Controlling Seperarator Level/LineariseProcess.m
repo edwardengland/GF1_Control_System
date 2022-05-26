4e193ad450da1f7144da405045185758d955c097
Edward England <edwardengland@hotmail.co.uk>
2022-05-26 21:50:44 +0100

    End of Week 2 Session 3

--- Week 2/Simulink/3. Controlling Seperarator Level/LineariseProcess.m
+++ Week 2/Simulink/3. Controlling Seperarator Level/LineariseProcess.m
@@ -1,16 +1,22 @@
-[sizes, x0, xnames] = x3_3_Linearisation_Ready([], [], [], 0);
+[sizes, x0, xnames] = y3_3_Linearisation_Ready([], [], [], 0)
 
 % States to Linearisation Around
 % L2: 1
 % X2: 25
 % P2: 50.5
-% Lag1: 194.7
-% Lag2: 208
-% Lag IC2: 2
-lin = linmod('x3_3_Linearisation_Ready', [1, 25, 50.5, 194.7, 208, 2], [0])
+% P100 Lag : 194.7
+% F200 Lag: 208
+% F2 Lag: 2
+lin = linmod('y3_3_Linearisation_Ready', [1, 2, 208, 194.7, 50.5, 25], [0])
 
 % Assemble linearised LTI system
 
-sys1 = ss(lin.a, lin.b, lin.c, lin.d)
+sys1 = ss(lin.a, -14*lin.b, lin.c, lin.d)
+% MULTIPLY B MATRIX BY PROPORTIONAL GAIN TO SEE EFFECTS
+
 
 bode(sys1)
+
+% 23db for 45deg phase margin
+% gain of 14
+
